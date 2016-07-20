ce6e807b09e34e6449510f7d5542ba40eb099b2a
Ema <ema@ema-no-macbook-air.local>
2016-07-21 08:02:27 +0900

    Initial Commit

---
+++ WinePiano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WinePiano
+//
+//  Created by Ema on 2016/07/21.
+//  Copyright © 2016年 Ema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
